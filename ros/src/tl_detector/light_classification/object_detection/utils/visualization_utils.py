0c828ab2bac301f98d1dcc9174007a8874667c2a
Vikramank <vikramank@hotmail.com>
2017-11-25 03:05:29 -0800

    Update visualization_utils.py

--- ros/src/tl_detector/light_classification/object_detection/utils/visualization_utils.py
+++ ros/src/tl_detector/light_classification/object_detection/utils/visualization_utils.py
@@ -434,6 +434,7 @@ def visualize_boxes_and_labels_on_image_array(image,
   box_to_color_map = collections.defaultdict(str)
   box_to_instance_masks_map = {}
   box_to_keypoints_map = collections.defaultdict(list)
+  class_name='N/A'
   if not max_boxes_to_draw:
     max_boxes_to_draw = boxes.shape[0]
   for i in range(min(max_boxes_to_draw, boxes.shape[0])):
